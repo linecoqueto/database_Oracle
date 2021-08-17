390fe59610a885d7f8ceecaf86a3c1dd3dcc8d89
Aline Coqueto <64856676+linecoqueto@users.noreply.github.com>
2021-08-16 21:12:14 -0300

    Create performance_estatistica_index_clustering_factor.sql

---
+++ performance_estatistica_index_clustering_factor.sql
@@ -0,0 +1,17 @@
+--quanto mais proximo o numero de blocos (blocks) do clustering factor MELHOR : isso significa que a chave do index ta mais juntinha.
+--quanto mais proximo o numero de linhas (num_rows) do clustering factor PIOR.
+
+--- Mesmo que eu esteja acessando apenas 2% dos dados da minha tabela, mas se os dados buscados estiverem muito espalhados, 
+--- pode ser mais interessante pro Oracle fazer um full table scan do que acesso por index.
+
+SELECT 
+T.TABLE_NAME,
+I.INDEX_NAME,
+I.CLUSTERING_FACTOR,
+T.BLOCKS,
+T.NUM_ROWS
+FROM DBA_TABLES, DBA_INDEXES I
+WHERE T.TABLE_NAME = I.TABLE_NAME
+AND T.OWNER = 'owner_name'
+AND T.TABLE = 'table_name'
+ORDER BY T.TABLE_NAME, I.INDEX_NAME;
